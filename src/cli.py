f15bcff12ce6e319aeff4fd674f339faaf0be3db
Alexey Tkachenko <a.tkachenko@innopolis.university>
2025-03-11 23:12:30 +0300

    Now we draw flow

--- src/cli.py
+++ src/cli.py
@@ -1,16 +1,21 @@
+from llama_index.utils.workflow import draw_most_recent_execution
+
 from src.workflow import AssistantFlow
 
 
 async def main():
+    w = AssistantFlow(timeout=60, verbose=False)
+
     print("You:")
 
     query = input()
 
-    w = AssistantFlow(timeout=60, verbose=False)
     result = await w.run(query=query)
 
     print("Result:")
     print(result)
+
+    draw_most_recent_execution(w, filename="assistant_flow_recent.html")
 
 
 if __name__ == "__main__":
