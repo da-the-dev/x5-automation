eb4163e9ff719da938e4bc626a77c7f106021628
Alexey Tkachenko <a.tkachenko@innopolis.university>
2025-03-11 16:21:23 +0300

    VLLM support

--- src/agentic/llm.py
+++ src/agentic/llm.py
@@ -1,10 +1,11 @@
 from langchain_openai import ChatOpenAI
 
-from .settings import agents_settings
+# from .settings import agents_settings
 
 
 # For now, we are using OpenAI's ChatGPT model, but in the future, we need to make model selection more configurable.
 llm: ChatOpenAI = ChatOpenAI(
-    api_key=agents_settings.openai_api_key,
-    model=agents_settings.openai_model
+    base_url="http://localhost:8000/v1",
+    api_key="token-123",
+    model="Vikhrmodels/Vikhr-Llama-3.2-1B-Instruct"
 )
