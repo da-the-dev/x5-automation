be748b2cfbac1af5b097729f8a5c42a86ebc5df6
RipYashok <yakov.marchenkov@yandex.ru>
2025-03-12 13:45:18 +0300

    name and fast del for base

--- base_up/base_up.sh
+++ base_up/base_up.sh
@@ -1,3 +1,3 @@
-docker run -p 6333:6333 -p 6334:6334 \
+docker run -d --rm --name full_database -p 6333:6333 -p 6334:6334 \
     -v "$(pwd)/qdrant_storage:/qdrant/storage:z" \
-    qdrant/qdrant
+    qdrant/qdrant 
