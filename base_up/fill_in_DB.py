db5145e6bcbe9e1af5bc2c80931eb642744af065
RipYashok <yakov.marchenkov@yandex.ru>
2025-03-12 11:38:01 +0300

    script for database filling

---
+++ base_up/fill_in_DB.py
@@ -0,0 +1,45 @@
+import torch
+import pandas as pd
+import sys
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+from qdrant_client.models import PointStruct
+
+vectors = torch.load("embeddings.pt",  weights_only=True)
+
+df_payload = pd.read_csv("qa_df_pairs_db.csv")
+
+payloads = df_payload.to_dict(orient="records")
+
+points = [
+    PointStruct(id=i, vector=vectors[i], payload=payloads[i])
+    for i in range(len(vectors))
+]
+
+client = QdrantClient(url=f"http://{sys.argv[1]}")
+
+client.delete_collection(collection_name="X5_database")
+client.create_collection(
+    collection_name="X5_database",
+    vectors_config=VectorParams(size=1024, distance=Distance.DOT),
+)
+
+
+
+batch_size = 100  # Установи подходящий размер
+for i in range(0, len(points), batch_size):
+    batch = points[i:i + batch_size]
+    client.upsert(collection_name="X5_database", wait=True, points=batch)
+
+
+print("Данные загружены")
+
+points = client.scroll(collection_name="X5_database", limit=1)
+
+if points:  # Проверяем, что список не пустой
+    
+    print("Первый элемент:")
+    print(points[0][0])
+
+else:
+    print("Список записей пуст")
