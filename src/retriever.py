26de5ec7dfa20228045013c0301a2adc3f071c01
aleksei <aleksei@Noutbuk-aleksei.local>
2025-03-12 13:06:12 +0300

    logic for encoding

--- src/retriever.py
+++ src/retriever.py
@@ -1,34 +1,44 @@
-import json
-import requests
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from qdrant_client import QdrantClient
+
+
+def encode_query(query_clean: str) -> list[float]:
+    # TODO set variables to config and init embedder_model one time
+    HF_MODEL_NAME = "elderberry17/USER-bge-m3-x5"
+    embedder_model = HuggingFaceEmbedding(model_name=HF_MODEL_NAME)
+    query_embedding = embedder_model.get_text_embedding(query_clean)
+    return query_embedding
+
+
+def retrieve_points(query_embedding: list[float]):
+    # TODO set variables to config and init client one time
+    QDRANT_QUERY_URL = "http://localhost:6333"
+    TOPN = 10
+    COLLECTION_NAME = "X5_database"
+
+    qdrant_client = QdrantClient(url=QDRANT_QUERY_URL)
+
+    search_result = qdrant_client.query_points(
+        collection_name=COLLECTION_NAME,
+        query=query_embedding,
+        with_payload=True,
+        limit=TOPN
+    ).points
+
+    return search_result
+
+
+def process_points(points: list[dict]) -> list[tuple[str, str]]:
+    qa_tuples = [(point['question_clear'], point['content_clear']) for point in points]
+    return qa_tuples
 
 
 def retriever(query_clean: str) -> list[tuple[str, str]]:
-    '''
-    We need to specify endpoints for:
-    - embedder model
-    - qdrant database
-    '''
+    query_embedding = encode_query(query_clean)
+    # search_result = retrieve_points(query_embedding)
+    # search_result_clear = process_points(search_result)
+
+    print(len(query_embedding))
+    # return search_result_clear
+    return [("q", "a")]
     
-    # TODO retrieve
-    # mock values
-    VLLM_HOST = "http://localhost:8000"
-    embedder_endpoint = f"{VLLM_HOST}/v1/embeddings"
-
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    # Define the data payload
-    data = {
-        "model": "elderberry17/USER-bge-m3-x5",
-        "input": [query_clean]
-    }
-
-    response = requests.post(embedder_endpoint, headers=headers, data=json.dumps(data))
-    if response.status_code == 200:
-        query_embedding = response.json().get('data', [])[0].get('embedding', [])
-        print(f"embedding len: {len(query_embedding)}")
-    else:
-        print(response.status_code)
-
-    # TODO search similar queries from qdrant
