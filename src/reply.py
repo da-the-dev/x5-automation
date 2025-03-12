35af0045ce9bfd85fd72bf209e9f32caa62b6592
Alexey Tkachenko <a.tkachenko@innopolis.university>
2025-03-12 13:02:29 +0300

    first reply

--- src/reply.py
+++ src/reply.py
@@ -2,16 +2,19 @@ async def reply(query_clean: str, qa: list[tuple[str, str]]) -> str:
     from llama_index.llms.openai_like import OpenAILike
 
     llm = OpenAILike(
-        api_base="http://localhost:8000/v1",
+        api_base="http://51.250.7.86:8000/v1",
         api_key="token-123",
         model="Vikhrmodels/Vikhr-Nemo-12B-Instruct-R-21-09-24",
+        max_tokens=512,
+        temperature=0.5,
     )
-    return await llm.acomplete(
-        (
-            "Запрос пользователя следующий:\n",
-            f"{query_clean}\n",
-            "Вот примеры вопрос и ответов:\n",
-            f"{"\n".join([q + "->" + a for q, a in qa])}\n",
-            "Дай ответ на первоначальный запрос"
-        )
+
+    query = (
+        "Запрос пользователя:\n"
+        f"{query_clean}\n"
+        "Вот примеры вопрос и ответов:\n"
+        f"{"\n".join([q + "->" + a for q, a in qa])}\n"
+        "Дай ответ на запрос пользователя в том же стиле, как в примерах"
     )
+
+    return await llm.acomplete(query)
