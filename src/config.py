83a709d899ee9d17736001a9771bd6b06c4d67d0
Alexey Tkachenko <a.tkachenko@innopolis.university>
2025-03-13 16:39:43 +0300

    Fixed config

--- src/config.py
+++ src/config.py
@@ -5,5 +5,12 @@ import os
 import yaml
 
 
-with open(os.path.join(os.getcwd(), "config.yaml"), "r") as stream:
+with open(
+    (
+        "/run/secrets/config.yaml"
+        if os.getenv("PROD")
+        else os.path.join(os.getcwd(), "config.yaml")
+    ),
+    "r",
+) as stream:
     config = yaml.safe_load(stream)
